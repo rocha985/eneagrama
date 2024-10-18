ee010b73b21f0ca1899d8b03f9e600c587140244
Rocha <arkhanticsbr@outlook.com>
2024-10-18 13:02:48 -0300

    fix popup

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const bodyParser = require("body-parser");
 const calculateScores = require("./calculateScores");
 const path = require("path");
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(express.static("public"));
+
+app.use(express.static(path.join(__dirname)));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "src", "public", "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.post("/calculate", (req, res) => {
@@ -20,7 +20,8 @@ app.post("/calculate", (req, res) => {
         const resultEneatipo = calculateScores(userAnswers);
         res.json({ resultEneatipo });
     } catch (error) {
-        res.status(500).json({ error: "Erro interno do servidor" });
+        console.error("Error processing request:", error);
+        res.status(500).send("Internal Server Error");
     }
 });
 

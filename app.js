626409c54fbebe27f98f9c60f141dd37e0030e56
Rocha <arkhanticsbr@outlook.com>
2024-10-22 18:40:11 -0300

    fix api

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const calculateScores = require("./calculateScores");
+const calculateScores = require("./api/calculateScores");
 const path = require("path");
 
 const app = express();
